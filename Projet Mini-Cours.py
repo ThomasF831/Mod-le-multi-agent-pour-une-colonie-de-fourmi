d6583716f81ea711c04dd7638cfbf99de2e2e984
ThomasF831 <114236206+ThomasF831@users.noreply.github.com>
2023-02-10 14:25:04 +0100

    Add files via upload

---
+++ Projet Mini-Cours.py
@@ -0,0 +1,108 @@
+import mesa
+import random
+import matplotlib.pyplot as plt
+
+T=[i for i in range(1001)]
+NourritureCollectée=[0]
+
+class Agent(mesa.Agent):
+    def __init__(self,unique_id,model):
+        super().__init__(unique_id,model)
+        self.nourriture=0
+        self.type_agent="Fourmi"
+        self.intensite=1
+    def step(self):
+        #pos=random.randint(1,self.model.grid.width-1),random.randint(1,self.model.grid.height-1)
+        C=self.model.grid.get_cell_list_contents([self.pos])
+        for ag in C:
+            if ag.type_agent=="Marqueur" and self.nourriture==1:
+                ag.intensite+=1
+            if ag.type_agent=="Nourriture":
+                self.nourriture=1
+            if ag.type_agent=="Fourmillière":
+                self.nourriture=0
+                self.model.nourriture_collectée+=1
+        N=self.model.grid.get_neighbors(self.pos,False,False,1)
+        L=[]
+        x,y=self.pos
+        for n in N:
+            if n.type_agent=="Marqueur":
+                if self.nourriture==0:
+                    for i in range(n.intensite):
+                        L.append(n.pos)
+                else:
+                    L.append(n.pos)
+        pos=random.choice(L)
+        self.model.grid.move_agent(self,pos)
+
+class Model(mesa.Model):
+    def __init__(self,N,width,height):
+            self.nourriture_collectée=0
+            self.num_agents=N
+            self.grid = mesa.space.MultiGrid(width, height, False)
+            self.schedule=mesa.time.RandomActivation(self)
+            n=width*height
+            pos=random.randint(0,self.grid.width-1),random.randint(0,self.grid.height-1)
+            a=Agent(n+N+2,self)
+            a.type_agent="Nourriture"
+            self.grid.place_agent(a,pos)
+            a=Agent(n+N+1,self)
+            a.type_agent="Fourmillière"
+            pos=self.grid.find_empty()
+            self.grid.place_agent(a,pos)
+            for i in range(width):
+                for j in range(height):
+                    a=Agent(i*width+j,self)
+                    a.type_agent="Marqueur"
+                    self.grid.place_agent(a,(i,j))
+            for i in range(self.num_agents):
+                a=Agent(n+i,self)
+                a.intensite=0
+                self.schedule.add(a)
+                self.grid.place_agent(a,pos)
+    def step(self):
+        self.schedule.step()
+        NourritureCollectée.append(self.nourriture_collectée)
+        if len(NourritureCollectée)==1000:
+            plt.plot(T,NourritureCollectée)
+            plt.show()
+
+def agent_portrayal(agent):
+    portrayal = {
+        "Shape": "circle",
+        "Color": "black",
+        "Filled": "true",
+        "Layer": 1,
+        "r": 0.5
+    }
+
+    if agent.type_agent=="Marqueur":
+        portrayal["Color"]="grey"
+        portrayal["r"]=0.2
+        portrayal["Layer"]=2
+
+    if agent.type_agent=="Nourriture":
+        portrayal["Shape"]="circle"
+        portrayal["Color"]="red"
+        portrayal["r"]=0.8
+        portrayal["Layer"]=0
+
+    if agent.type_agent=="Fourmillière":
+        portrayal["Shape"]="circle"
+        portrayal["Color"]="green"
+        portrayal["r"]=0.8
+        portrayal["Layer"]=0
+
+    if agent.type_agent=="Fourmi" and agent.nourriture==1:
+        portrayal["Color"]="blue"
+
+    return portrayal
+
+grid = mesa.visualization.CanvasGrid(agent_portrayal, 12, 12, 500, 500)
+
+server = mesa.visualization.ModularServer(
+    Model, [grid], "Model", {"N": 80, "width": 12, "height": 12}
+)
+
+server.port = 8595  # The default
+server.launch()
